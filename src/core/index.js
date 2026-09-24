5ffc87ad6235ae43c3f6a63165837ad5395d37ce
OpenVibers <Contact@OpenVibe.Network>
2026-09-24 15:19:20 -0700

    v0.9.1: SDK_VERSION and the browser bundle match the release (v0.9.0 reported 0.8.0); same API as 0.9.0

--- src/core/index.js
+++ src/core/index.js
@@ -8,7 +8,7 @@ const { paginate, offsetPager } = require('./paginate');
 const semver = require('./semver');
 
 /** The package version (test/version.test.js keeps it equal to package.json). */
-const SDK_VERSION = '0.8.0';
+const SDK_VERSION = '0.9.1';
 
 module.exports = {
     createClient,
